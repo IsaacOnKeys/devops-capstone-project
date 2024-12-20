c44218cd643c0dbb3a5b346821728a9241c993dc
IsaacOnKeys <iamnoticandy@protonmail.com>
2024-12-20 06:19:51 -0500

    linted, debuged secrutiy headers test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -199,20 +199,20 @@ class TestAccountService(TestCase):
         headers = {
             'X-Frame-Options': 'SAMEORIGIN',
             'X-Content-Type-Options': 'nosniff',
-            'Content-Security-Policy': 'default-src \'self\'; \
-                object-src \'none\'',
+            'Content-Security-Policy': "default-src 'self'; object-src 'none'",
             'Referrer-Policy': 'strict-origin-when-cross-origin'
         }
         for key, value in headers.items():
-            self.assertEqual(response.headers.get(key), value)
+            actual_value = response.headers.get(key)
+            self.assertEqual(
+                " ".join(actual_value.split()),
+                " ".join(value.split()))
 
     def test_cors_security(self):
         """It should return a CORS header"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
         # Check for the CORS header
-        self.assertEqual(
-            response.headers.get('Access-Control-Allow-Origin'),
-            '*')
+        cors_header = response.headers.get('Access-Control-Allow-Origin')
+        self.assertEqual(cors_header, '*')
